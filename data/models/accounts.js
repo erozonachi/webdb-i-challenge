d7b92ea03aeacc2dee9ad6bf916b94b374c55ee9
erozonachi <erozonachi.james@gmail.com>
2019-07-15 16:33:19 +0100

    Modify accounts model's insert function

--- data/models/accounts.js
+++ data/models/accounts.js
@@ -2,7 +2,9 @@ const db = require('../dbConfig');
 
 module.exports = {
   insert: ({ name, budget }) => {
-    return db('accounts').insert({ name, budget }, ['id', 'name', 'budget']);
+    return db('accounts')
+    .insert({ name, budget }, ['id', 'name', 'budget'])
+    .then(([id]) => this.get(id));
   },
 
   get: (id = null) => {
