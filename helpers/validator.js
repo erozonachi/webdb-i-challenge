311601ef55fcbe12f8a227d1dd9e87db4ab28b18
erozonachi <erozonachi.james@gmail.com>
2019-07-15 15:29:49 +0100

    Add validate-id function to validator module

--- helpers/validator.js
+++ helpers/validator.js
@@ -1,3 +1,17 @@
+const acctModel = require('../data/models/accounts');
 module.exports = {
-  
+  validateId: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+
+      const account = await acctModel.get(id);
+      if(account[0] && account[0].name) {
+        req.account = account;
+        return next();
+      }
+      res.status(400).json({ message: 'Invalid account id' });
+    } catch(error) {
+      res.status(500).json({ error: 'server error' });
+    }
+  },
 }
