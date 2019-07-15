0842d707479a26cc2fa73516e3c8aac25325d88c
erozonachi <erozonachi.james@gmail.com>
2019-07-15 15:48:46 +0100

    Add create function to accounts controller

--- controllers/accountsController.js
+++ controllers/accountsController.js
@@ -1,5 +1,12 @@
 const acctModel = require('../data/models/accounts');
 
 module.exports = {
-  
+  create: async (req, res) => {
+    try {
+      const account = await acctModel.insert(req.body);
+      res.status(201).json(account[0]);
+    } catch(error) {
+      res.status(500).json({ error: 'server error' });
+    }
+  },
 }
