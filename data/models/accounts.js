7e639acefc03d0b852424d154ffe3c0bd34b32f9
erozonachi <erozonachi.james@gmail.com>
2019-07-15 15:08:38 +0100

    Add get function to accounts model

--- data/models/accounts.js
+++ data/models/accounts.js
@@ -3,5 +3,13 @@ const db = require('../dbConfig');
 module.exports = {
   insert: ({ name, budget }) => {
     return db('accounts').insert({ name, budget }, ['id', 'name', 'budget']);
-  }
+  },
+
+  get: (id = null) => {
+    if(id) {
+      return db('accounts')
+        .where({ id });
+    }
+    return db('accounts');
+  },
 };
